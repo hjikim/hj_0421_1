364e176c44a36c8a95566d914d4a0ebd7a174c8d
evicpie <pwoqhr@naver.com>
2019-04-21 12:54:39 +0900

    Initial commit

---
+++ app/src/main/java/com/tj/hj_0421_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tj.hj_0421_1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityAppDetailBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/tj/hj_0421_1/app.java
@@ -0,0 +1,12 @@
+package com.tj.hj_0421_1;
+
+public class app {
+
+    public int rank; //순위
+    public String title; //앱제목
+    public String companyName; //회사명
+    public int userRanking; //별 개수
+    public int price; //구매 가격
+    public boolean isMine; //내가 구매 했는 지 여부.  True: 설치된 항목, False: 가격
+    
+}
